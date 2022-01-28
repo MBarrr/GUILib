d9d4b006c33669559f5cbb64edb928804d0721d3
MBarrr <62114563+MBarrr@users.noreply.github.com>
2022-01-28 13:31:53 +0000

    mov

--- src/main/java/mbarrr/github/guilib/GUILib.java
+++ src/main/java/mbarrr/github/guilib/GUILib.java
@@ -1,45 +1,34 @@
 package mbarrr.github.guilib;
 
-import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.persistence.PersistentDataContainer;
 import org.bukkit.persistence.PersistentDataType;
-import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.plugin.Plugin;
 
-public final class GUILib extends JavaPlugin {
+import java.util.List;
+
+public final class GUILib{
+
+    Plugin instance;
+
+    public GUILib(Plugin instance){
+        this.instance = instance;
+    }
+
+
 
     private String pathStart = "guilib_";
 
-    NamespacedKey arrowKey = new NamespacedKey(this, pathStart+"arrow");
+    NamespacedKey arrowKey = new NamespacedKey(instance, pathStart+"arrow");
 
-    private ItemStack leftArrow;
-    private ItemStack rightArrow;
-    private ItemStack parentGUIArrow;
+    private ItemStack leftArrow = giveItemTags(arrowKey, 0, Material.MUSIC_DISC_CAT);
+    private ItemStack rightArrow = giveItemTags(arrowKey, 1, Material.MUSIC_DISC_CAT);
+    private ItemStack parentGUIArrow = giveItemTags(arrowKey, 2, Material.MUSIC_DISC_BLOCKS);
 
-    @Override
-    public void onEnable() {
-
-
-
-        loadItems();
-        this.getCommand("Debug").setExecutor(new Debug(new secGUI(9, "test", this, null)));
-    }
-
-    @Override
-    public void onDisable() {
-        // Plugin shutdown logic
-    }
-
-    private void loadItems(){
-        leftArrow = giveItemTags(arrowKey, 0, Material.MUSIC_DISC_CAT);
-        rightArrow = giveItemTags(arrowKey, 1, Material.MUSIC_DISC_CAT);
-        parentGUIArrow = giveItemTags(arrowKey, 2, Material.MUSIC_DISC_BLOCKS);
-    }
-
-    private ItemStack giveItemTags(NamespacedKey key, int tag, Material material){
+    public ItemStack giveItemTags(NamespacedKey key, int tag, Material material){
 
         ItemStack itemStack = new ItemStack(material);
         ItemMeta itemMeta = itemStack.getItemMeta();
@@ -48,6 +37,33 @@ public final class GUILib extends JavaPlugin {
         itemStack.setItemMeta(itemMeta);
 
         return itemStack;
+    }
+
+    public ItemStack renameItem(ItemStack item, String name){
+        ItemMeta itemMeta = item.getItemMeta();
+        itemMeta.setDisplayName(name);
+        item.setItemMeta(itemMeta);
+        return item;
+    }
+
+    public ItemStack setItemLore(ItemStack item, List<String> lore){
+        ItemMeta itemMeta = item.getItemMeta();
+        itemMeta.setLore(lore);
+        item.setItemMeta(itemMeta);
+        return item;
+    }
+
+    public int getItemAction(ItemStack item, NamespacedKey key){
+        ItemMeta itemMeta = item.getItemMeta();
+        PersistentDataContainer container = itemMeta.getPersistentDataContainer();
+        return(container.get(key, PersistentDataType.INTEGER));
+
+    }
+
+    public boolean hasItemAction(ItemStack item, NamespacedKey key){
+        ItemMeta itemMeta = item.getItemMeta();
+        PersistentDataContainer container = itemMeta.getPersistentDataContainer();
+        return container.has(key, PersistentDataType.INTEGER);
     }
 
     public NamespacedKey getArrowKey(){
@@ -64,9 +80,5 @@ public final class GUILib extends JavaPlugin {
 
     public  ItemStack getParentGUIArrow() {
         return parentGUIArrow;
-    }
-
-    public static GUILib getInstance(){
-        return (GUILib) Bukkit.getPluginManager().getPlugin("GUILib");
     }
 }
